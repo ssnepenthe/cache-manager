2f5238932573ce9867e612940ed03a1bfade8047
ssnepenthe <ssnepenthe@users.noreply.github.com>
2015-11-14 11:03:56 -0700

    Update plugin bootstrap for new plugin and toolbar classes.

--- cache-manager.php
+++ cache-manager.php
@@ -34,10 +34,67 @@ if ( file_exists( $plugin_root . '/vendor/autoload.php' ) ) {
  * Initializes the plugin on the init hook.
  */
 function init() {
+	if ( ! is_admin() && ! is_admin_bar_showing() ) {
+		return;
+	}
+
 	$manager = new CacheManager( 'cache-manager', '0.1.0' );
 
-	$manager->add_cache_class( 'fastcgi', 'SSNepenthe\\CacheManager\\NginxFastCGICache' );
+	$manager->add_cache_class(
+		'fastcgi',
+		'SSNepenthe\\CacheManager\\NginxFastCGICache'
+	);
 	$manager->set_default_cache_class( 'fastcgi' );
+
+	$classes = [ 'cache-manager-icon' ];
+
+	if ( $instance = $manager->current_instance() ) {
+		if ( $instance->exists() ) {
+			$classes[] = 'exists';
+		} else {
+			$classes[] = 'does-not-exist';
+		}
+	}
+
+	$manager->add_toolbar_nodes( [
+		[
+			'id'         => 'ssn-cache-manager',
+			'title'      => sprintf(
+				'Cache<div class="%1$s"></div>',
+				implode( ' ', $classes )
+			),
+			'display-cb' => '__return_true',
+			'no-href'    => true,
+		],
+		[
+			'id'         => 'ssn-refresh-cache',
+			'title'      => 'Refresh Cache',
+			'action-cb'  => [ $manager, 'refresh_callback' ],
+			'display-cb' => '__return_true',
+		],
+		[
+			'id'         => 'ssn-delete-cache',
+			'title'      => 'Delete Cache',
+			'action-cb'  => [ $manager, 'delete_callback' ],
+			'display-cb' => '__return_true',
+		],
+		[
+			'id'         => 'ssn-create-cache',
+			'title'      => 'Create Cache',
+			'action-cb'  => [ $manager, 'create_callback' ],
+			'display-cb' => '__return_true',
+		],
+		[
+			'id'         => 'ssn-flush-cache',
+			'href'       => add_query_arg(
+				[ 'action' => 'ssn-flush-cache' ],
+				admin_url( 'index.php' )
+			),
+			'title'      => 'Flush Cache',
+			'action-cb'  => [ $manager, 'flush_callback' ],
+			'display-cb' => '__return_true',
+		],
+	] );
 
 	// Allow user to register new cache handlers and change default.
 	do_action( __NAMESPACE__ . '\\init', $manager );
@@ -45,8 +102,7 @@ function init() {
 	try {
 		$manager->init();
 	} catch ( \Exception $e ) {
-		// Should handle this better.
-		echo $e->getMessage();
+		// How to handle???
 	}
 }
 add_action( 'init', __NAMESPACE__ . '\\init' );
